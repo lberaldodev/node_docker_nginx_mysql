fbdbc6ea8dacc75ad4035496386ca95079ca0f8e
Lucas da Silva Beraldo <lberaldo01@gmail.com>
2022-06-28 18:30:02 -0300

    feat: create and list peoples from database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const { connection } = require('./infra/database/connection');
+const PeopleRepository = require('./repository/people');
 
 require('dotenv').config()
  
@@ -9,10 +10,19 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
-  const peopleRepository = require('./repository/people');
-  const peopleName = req.query.name || 'John Doe'
-  await peopleRepository(connection).create(peopleName);
-  return res.send(`Hello World! ${peopleName}`)
+  const repository = PeopleRepository(connection);
+
+  const peopleName = req.query.name;
+
+  if (peopleName) {
+    await repository.create(peopleName);
+  }
+
+  const peoplesList = await repository.list();
+
+  const peoplesText = peoplesList.map(people => people.name).join(', <br/>');
+
+  return res.send(`Welcome! List of users in database (${peoplesList.length}) <br/> ${peoplesText}`)
 })
 
 app.listen(port, () => {
