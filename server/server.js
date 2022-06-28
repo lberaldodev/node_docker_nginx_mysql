247e2d3b0ddf1a241674462e268b09f406f18153
Lucas da Silva Beraldo <lberaldo01@gmail.com>
2022-06-27 23:04:14 -0300

    feat: example with database connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 
 const express = require('express')
+const { connection } = require('./infra/database/connection');
 
 require('dotenv').config()
-
+ 
 const app = express()
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 3000
 
-app.get('/', (_, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  const peopleRepository = require('./repository/people');
+  const peopleName = req.query.name || 'John Doe'
+  await peopleRepository(connection).create(peopleName);
+  return res.send(`Hello World! ${peopleName}`)
 })
 
 app.listen(port, () => {
