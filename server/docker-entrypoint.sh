1e65f64f9efe8102fbb9052d4afe288ec256f395
Lucas da Silva Beraldo <lberaldo01@gmail.com>
2022-07-12 22:39:06 -0300

    chore: add generate to prisma inside entrypoint

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 echo "wait db server"
 dockerize -wait tcp://db:3306 -timeout 20s
 
+echo "generate prisma client"
+npx prisma generate --schema=./src/infra/database/prisma/schema.prisma
+
 echo "start node server"
 yarn start-dev
