42cf91d2214803854d5d1641ae839d8381c1fc54
Lucas da Silva Beraldo <lberaldo01@gmail.com>
2022-07-14 09:12:10 -0300

    feat: add migration to entrypoint

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -4,5 +4,8 @@ dockerize -wait tcp://db:3306 -timeout 20s
 echo "generate prisma client"
 npx prisma generate --schema=./src/infra/database/prisma/schema.prisma
 
+echo "migrate prisma db" 
+npx prisma migrate deploy --schema=./src/infra/database/prisma/schema.prisma
+
 echo "start node server"
 yarn start-dev
