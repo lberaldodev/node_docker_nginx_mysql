d008ab347983ef82e18144f9671ce643ecd78b72
Lucas da Silva Beraldo <lberaldo01@gmail.com>
2022-07-16 17:05:27 -0300

    feat: add swagger middleware

--- server/src/infra/server/index.ts
+++ server/src/infra/server/index.ts
@@ -3,9 +3,15 @@ import express from "express";
 import dotenv from "dotenv";
 import routes from "../../interfaces/routes";
 
+const swaggerDocument = require('../../docs/api-spec.json');
+const swaggerUi = require('swagger-ui-express');
+
 dotenv.config();
 
 const app = express();
+
+/* Swagger */
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.SERVER_PORT || 3000;
 
